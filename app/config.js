bc786da1bb1f50dff73ffcded0c63c0da4c6b623
Dominic Norris <you@example.com>
2020-11-16 13:24:13 +0000

    Added initial flow for adding new bank details

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Name of service a user has come from',
 
   // Default port that prototype runs on
   port: '3000',
